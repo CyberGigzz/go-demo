e1a8ca567c61a72ebe5dbcbe26e6a325d48c0397
Giga <gigakatamadze0@gmail.com>
2024-06-21 18:17:17 +0400

    Set up a live reloader

--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r) // Handle undefined routes
+	}
+
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io">jon@calhoun.io</a></p>`)
+
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("started the server on :3000...")
 	http.ListenAndServe(":3000", nil)
