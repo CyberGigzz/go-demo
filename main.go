b6a1b7badc69e30a2f496e7491896ddf39e328b8
Giga <gigakatamadze0@gmail.com>
2024-06-22 01:08:14 +0400

    Added chi HTTP router to the go code

--- main.go
+++ main.go
@@ -3,24 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome  to my awesome site!</h1>")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.NotFound(w, r) // Handle undefined routes
-	}
-
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,9 +19,13 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("started the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
