2136ae926a0e84f9ef2ce5725e8c83594f0fd4a0
Giga <gigakatamadze0@gmail.com>
2024-07-09 11:48:42 +0400

    Postgress config

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,15 +1,43 @@
 package main
 
-import "errors"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s \"+\n\t\t\"user=%s "+
+		"password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User,
+		cfg.Password, cfg.Database, cfg.SSLMode)
+}
 
 func main() {
-
-}
-
-func Conncect() error {
-	return errors.New("connection failed")
-}
-
-func CreateUser() error {
-	return errors.New("connection failed")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected")
 }
