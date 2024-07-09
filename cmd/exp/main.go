dfa9248cf5f5ff3321e3b5b9f16fd946348be0d3
Giga <gigakatamadze0@gmail.com>
2024-07-09 11:50:07 +0400

    Postgress config

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -16,7 +16,7 @@ type PostgresConfig struct {
 }
 
 func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s \"+\n\t\t\"user=%s "+
+	return fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User,
 		cfg.Password, cfg.Database, cfg.SSLMode)
 }
